d21543247b1193417a47b3547edfe6bfabd24f6c
Souheyl LORICOURT <souheyl.loricourt@lyc-edouard-branly-dreux.ac-orleans-tours.fr>
2026-01-22 15:54:33 +0100

    maj partie tkinter+ fichier image

--- partie tkinter.py
+++ partie tkinter.py
@@ -1,27 +1,31 @@
 import tkinter as tk
+from tkinter import PhotoImage
+from time import sleep
+from PIL import Image, ImageTk
 
-def grille_cliquable_sans_doublon(canvas, L, H, taille_case=40, ox=50, oy=100):
+# ================== LABYRINTHE ==================
+
+def grille_cliquable_sans_doublon(canvas, fenetre, L, H, taille_case=50, ox=50, oy=100):
     """
-    Quadrillage L x H :
-    - aucune arête dupliquée
-    - chaque arête est cliquable
-    - clic : plein <-> pointillé
+    Crée une grille L x H cliquable :
+    - lignes pleines ou pointillées
+    - possibilité de verrouiller certaines arêtes
     """
 
     def basculer(event):
         arete = canvas.find_withtag("current")[0]
         tags = canvas.gettags(arete)
-        
+
         if "Bordure" in tags or "verrouille" in tags:
             return
 
         if "selection" in tags:
-            canvas.dtag("selection", arete)
+            canvas.dtag(arete, "selection")
             canvas.itemconfig(arete, dash=())
         else:
             canvas.addtag_withtag("selection", arete)
-            canvas.itemconfig(arete, dash = (4,4))
-        
+            canvas.itemconfig(arete, dash=(4, 4))
+
         if "pointille" in tags:
             canvas.itemconfig(arete, dash=())
             canvas.dtag(arete, "pointille")
@@ -41,15 +45,9 @@ def grille_cliquable_sans_doublon(canvas, L, H, taille_case=40, ox=50, oy=100):
             tags = ["arete", "plein"]
             if i == 0 or i == L:
                 tags.append("Bordure")
-                
-            arete = canvas.create_line(
-                x, y1, x, y2,
-                width=2,
-                dash=(),
-                tags= tuple(tags)
-            )
+
+            arete = canvas.create_line(x, y1, x, y2, width=2, tags=tuple(tags))
             canvas.tag_bind(arete, "<Button-1>", basculer)
-           
 
     # --- Arêtes horizontales ---
     for j in range(H + 1):
@@ -62,43 +60,117 @@ def grille_cliquable_sans_doublon(canvas, L, H, taille_case=40, ox=50, oy=100):
             if j == 0 or j == H:
                 tags.append("Bordure")
 
-            arete = canvas.create_line(
-                x1, y, x2, y,
-                width=2,
-                dash=(),
-                tags=tuple(tags)
-            )
+            arete = canvas.create_line(x1, y, x2, y, width=2, tags=tuple(tags))
             canvas.tag_bind(arete, "<Button-1>", basculer)
 
-
-
-    # --- Verrouillage des murs---
-
-    def valider_selection(canvas):
+    
+    # --- Verrouillage ---
+    def valider_selection(bouton):
         for arete in canvas.find_withtag("selection"):
             canvas.dtag(arete, "selection")
             canvas.addtag_withtag("verrouille", arete)
             canvas.itemconfig(arete, fill="gray")
+        bouton.config(state="disabled")
 
+    bouton = tk.Button(
+        fenetre,
+        text="Valider les murs du labyrinthe",
+        font=("", 20),
+        relief="solid",
+        command=lambda: valider_selection(bouton)
+    )
+    bouton.pack(pady=10)
 
-   #---Bouton verrouillage des murs du labyrinth---
-            
+# ================== RESTART ==================
 
-    bouton = tk.Button(fenetre, text = "Valider les murs du Labyrinth", command=lambda: valider_selection(canvas))
-    bouton.pack()
+def restart():
+    #TODO
+    pass
 
-    
+# ================== ECRAN DE SELECTION ==================
 
+def lancer_labyrinthe():
+    lignes = entree_ligne.get()
+    colonnes = entree_colonne.get()
+
+    if not (lignes.isdigit() and int(lignes) <= 15 and colonnes.isdigit() and int(colonnes) <= 15):
+        label_invalide = tk.Label(root, text="Entrée invalide!", fg="red", font=("", 15, "bold"))
+        label_invalide.pack()
         
+        print("Entrée invalide")
+        return
+
+    L, H = int(colonnes), int(lignes)
+    taille_case = 50
+
+    for widget in root.winfo_children():
+        widget.destroy()
+
+    frame_parent = tk.Frame(root)
+    frame_parent.pack(fill="both", expand=True)
+
+    canvas_width = L * taille_case + 100
+    canvas_height = H * taille_case + 100
+
+    label_titre_2 = tk.Label(frame_parent, text="Modifiez votre grille!", font=("", 60, "bold"))
+    label_titre_2.pack()
+    
+    frame_canvas = tk.Frame(frame_parent, width=canvas_width, height=canvas_height)
+    frame_canvas.pack(expand=True)
+    frame_canvas.pack_propagate(False)
+
+    canvas = tk.Canvas(frame_canvas, width=canvas_width, height=canvas_height, bg="white")
+    canvas.pack(fill="both", expand=True)
+
+    ox, oy = 50, 50
 
     
+    grille_cliquable_sans_doublon(canvas, root, L, H, taille_case=taille_case, ox=ox, oy=oy)
 
 
+# ================== PROGRAMME PRINCIPAL ==================
 
-fenetre = tk.Tk()
-canvas = tk.Canvas(fenetre, width=500, height=400, bg="white")
-canvas.pack()
+root = tk.Tk()
+root.title("Labyrinthe")
+root.resizable(False, False) 
+root.state("zoomed")
 
-grille_cliquable_sans_doublon(canvas, L=6, H=5, taille_case=50)
+restart_image = Image.open("image/restart.png")
+restart_image_width = 50
+restart_image_height = 50
+restart_image_resize = restart_image.resize((restart_image_width, restart_image_height))
+restart_image_tk = ImageTk.PhotoImage(restart_image_resize)
 
-fenetre.mainloop()
+
+bouton_reset = tk.Button(root, image=restart_image_tk, width=restart_image_width, height=restart_image_height, relief="solid",
+                         command=restart)
+bouton_reset.place(relx=0.99, rely=0.99, anchor="se")
+
+label_titre = tk.Label(root, text="Générer une grille", font=("", 60, "underline", "bold"))
+label_sous_titre = tk.Label(root, text="(Maximum: 15x15)", fg="blue", font=("", 30, "bold"))
+label_sous_titre_2 = tk.Label(root, text=" ", font=("", 10))
+
+label_lignes = tk.Label(root, text="Nombre de lignes", font=("", 25))
+entree_ligne = tk.Entry(root, borderwidth=3, relief="solid", highlightthickness=5, width=8, font=("", 50), justify="center")
+
+label_colonnes = tk.Label(root, text="Nombre de colonnes", font=("", 25))
+entree_colonne = tk.Entry(root, borderwidth=3, relief="solid", highlightthickness=5, width=8, font=("", 50), justify="center")
+
+bouton_confirmer = tk.Button(
+    root,   
+    text="Confirmer",
+    font=("", 20),
+    relief="solid",
+    command=lancer_labyrinthe
+)
+
+label_titre.pack(pady=20)
+label_sous_titre.pack()
+label_sous_titre_2.pack()
+label_lignes.pack()
+entree_ligne.pack()
+label_colonnes.pack()
+entree_colonne.pack()
+bouton_confirmer.pack(pady=20)
+
+root.mainloop()
